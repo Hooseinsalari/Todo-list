aa16c6436be24fe91750d49a34a2861e1261d320
Hossein Salari <hossein.salari002@gmail.com>
2024-01-13 16:04:30 +0330

    feat: implement db connection and user model

---
+++ models/User.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const schema = mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  userName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    required: true, // User || Admin
+  },
+});
+
+const userModel = mongoose.models.User || mongoose.model("User", schema);
+
+export default userModel;
---
+++ utils/db.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const connectToDB = async () => {
+  try {
+    if (mongoose.connections[0].readyState) {
+      return false;
+    }
+
+    await mongoose.connect("mongodb://localhost:27017/next-auth");
+    console.log("Connect to db has successfully");
+  } catch (error) {
+    console.log("Connect to db has failed, Error =>", error);
+  }
+};
+
+export default connectToDB;
