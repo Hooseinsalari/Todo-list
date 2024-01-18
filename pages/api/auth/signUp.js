cbffd66d68fba5c131c58c44ceec5bf5e4f84229
Hossein Salari <hossein.salari002@gmail.com>
2024-01-18 16:41:33 +0330

    feat: check user exist or not

--- pages/api/auth/signUp.js
+++ pages/api/auth/signUp.js
@@ -1,1 +1,52 @@
-export default function handler(req, res) {}
+import UserModel from "@/models/User";
+import connectToDB from "@/utils/db";
+
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    return false;
+  }
+
+  try {
+    connectToDB();
+
+    const { firstName, lastName, userName, email, password } = req.body;
+
+    if (
+      !firstName.trim() &&
+      !lastName.trim() &&
+      !userName.trim() &&
+      !email.trim() &&
+      !password.trim()
+    ) {
+      return res.status(422).json({ message: "Data is not Valid !!!" });
+    }
+
+    const isUserExist = await UserModel.findOne({
+      $or: [
+        { email: email },
+        { userName: userName },
+      ],
+    });
+
+    if(isUserExist) {
+      return res.status(422).json({message: "User already exist !"})
+    }
+
+    console.log("Check user exist:", isUserExist);
+
+    await UserModel.create({
+      firstName,
+      lastName,
+      userName,
+      email,
+      password,
+      role: "USER",
+    });
+
+    return res.status(201).json({ message: "User create Successfully" });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Unknown Internal Server Error", error });
+  }
+}
