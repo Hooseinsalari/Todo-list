08b8190323bf8fc5be5ed6e8c727f220d165d59e
Hossein Salari <hossein.salari002@gmail.com>
2024-01-19 18:36:03 +0330

    feat: impelment sign up page

--- pages/signup/index.js
+++ pages/signup/index.js
@@ -1,28 +1,113 @@
-import React from "react";
+import { useRouter } from "next/router";
+import React, { useState } from "react";
 
 function Index() {
+  const [user, setUser] = useState({
+    firstName: "",
+    lastName: "",
+    userName: "",
+    email: "",
+    password: "",
+  });
+
+  const router = useRouter()
+
+  const formHandler = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await fetch("/api/auth/signUp", {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        method: "POST",
+        body: JSON.stringify(user),
+      });
+
+      if (res.status === 201) {
+        console.log("user create successfully");
+
+        setUser({
+          firstName: "",
+          lastName: "",
+          userName: "",
+          email: "",
+          password: "",
+        });
+
+        alert("user create successfully")
+
+        router.replace("/dashboard")
+      } else if(res.status === 422) {
+        alert("User already exist !")
+      }
+    } catch (error) {
+      console.log(error.message);
+      // console.log("sign up faild, error:", error);
+    }
+  };
+
   return (
     <div className="box">
       <h1 align="center">SignUp Form</h1>
-      <form role="form" method="post">
+      <form onSubmit={submitHandler}>
         <div className="inputBox">
-          <input type="text" autoComplete="off" required />
+          <input
+            name="firstName"
+            value={user.firstName}
+            onChange={formHandler}
+            type="text"
+            autoComplete="off"
+            required
+          />
           <label>Firstname</label>
         </div>
         <div className="inputBox">
-          <input type="text" autoComplete="off" required />
+          <input
+            name="lastName"
+            value={user.lastName}
+            onChange={formHandler}
+            type="text"
+            autoComplete="off"
+            required
+          />
           <label>Lastname</label>
         </div>
         <div className="inputBox">
-          <input type="text" autoComplete="off" required />
+          <input
+            name="userName"
+            value={user.userName}
+            onChange={formHandler}
+            type="text"
+            autoComplete="off"
+            required
+          />
           <label>Username</label>
         </div>
         <div className="inputBox">
-          <input type="email" autoComplete="off" required />
+          <input
+            name="email"
+            value={user.email}
+            onChange={formHandler}
+            type="email"
+            autoComplete="off"
+            required
+          />
           <label>Email</label>
         </div>
         <div className="inputBox">
-          <input type="password" autoComplete="off" required />
+          <input
+            name="password"
+            value={user.password}
+            onChange={formHandler}
+            type="password"
+            autoComplete="off"
+            required
+          />
           <label>Password</label>
         </div>
 
