03d6a7cefe2c17563fc70da2a27edc4ac43dc996
Hossein Salari <hossein.salari002@gmail.com>
2024-01-20 15:49:38 +0330

    feat: ssr route protection in admin panel

--- pages/p-admin/index.js
+++ pages/p-admin/index.js
@@ -1,7 +1,54 @@
+import UserModel from "@/models/User";
+import { verifyToken } from "@/utils/auth";
+import connectToDB from "@/utils/db";
 import React from "react";
 
-function PAdmin() {
-  return <h1>Welcome To Admin Panel ❤️</h1>;
+function PAdmin({ firstName, lastName }) {
+  return (
+    <h1>
+      Welcome To Admin Panel, {firstName} {lastName} ❤️
+    </h1>
+  );
 }
 
 export default PAdmin;
+
+export async function getServerSideProps(context) {
+  connectToDB();
+  const { token } = context.req.cookies;
+
+  if (!token) {
+    return {
+      redirect: {
+        destination: "/signin",
+      },
+    };
+  }
+
+  const tokenPayload = verifyToken(token);
+
+  if (!tokenPayload) {
+    return {
+      redirect: {
+        destination: "/signin",
+      },
+    };
+  }
+
+  const userData = await UserModel.findOne(
+    { email: tokenPayload.email },
+    "firstName lastName role"
+  );
+
+  if (userData.role !== "ADMIN") {
+    return {
+      redirect: {
+        destination: "/",
+      },
+    };
+  }
+
+  return {
+    props: JSON.parse(JSON.stringify(userData)),
+  };
+}
