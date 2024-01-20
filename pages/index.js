85026c2e7caeb389258a5835ef81f7e6a5105864
Hossein Salari <hossein.salari002@gmail.com>
2024-01-20 15:30:09 +0330

    feat: display sidebar items base on user logged in

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
 
 import "@fortawesome/fontawesome-svg-core/styles.css";
@@ -13,61 +13,84 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 
 function Index() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userRole, setUserRole] = useState(null);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const res = await fetch("/api/auth/me");
+      if (res.status === 200) {
+        setIsLoggedIn(true);
+        const { data } = await res.json();
+        setUserRole(data.role);
+      }
+    };
+
+    fetchUser();
+  }, []);
+
   return (
     <div className="container">
       <aside className="sidebar">
         <h3 className="sidebar-title">Sidebar</h3>
 
         <ul className="sidebar-links">
-          <>
-            {/* User is login */}
-            <li>
-              <Link href="/dashboard">
-                <span>
-                  <FontAwesomeIcon icon={faBars} />
-                </span>
-                Dashboard
-              </Link>
-            </li>
-            <li>
-              <Link href="#">
-                <span>
-                  <FontAwesomeIcon icon={faSignOut} />
-                </span>
-                Logout
-              </Link>
-            </li>
-            {/* User is login */}
-          </>
-          <>
-            {/* User not login */}
-            <li>
-              <Link href="/signin">
-                <span>
-                  <FontAwesomeIcon icon={faSignIn} />
-                </span>
-                Sign in
-              </Link>
-            </li>
-            <li>
-              <Link href="/signup">
-                <span>
-                  <FontAwesomeIcon icon={faSignIn} />
-                </span>
-                Sign up
-              </Link>
-            </li>
-            {/* User not login */}
-          </>
+          {isLoggedIn ? (
+            <>
+              {/* User is login */}
+              <li>
+                <Link href="/dashboard">
+                  <span>
+                    <FontAwesomeIcon icon={faBars} />
+                  </span>
+                  Dashboard
+                </Link>
+              </li>
+              <li>
+                <Link href="#">
+                  <span>
+                    <FontAwesomeIcon icon={faSignOut} />
+                  </span>
+                  Logout
+                </Link>
+              </li>
+              {/* User is login */}
+            </>
+          ) : (
+            <>
+              {/* User not login */}
+              <li>
+                <Link href="/signin">
+                  <span>
+                    <FontAwesomeIcon icon={faSignIn} />
+                  </span>
+                  Sign in
+                </Link>
+              </li>
+              <li>
+                <Link href="/signup">
+                  <span>
+                    <FontAwesomeIcon icon={faSignIn} />
+                  </span>
+                  Sign up
+                </Link>
+              </li>
+
+              {/* User not login */}
+            </>
+          )}
+
           {/* User is login & admin */}
-          <li>
-            <Link href="/p-admin">
-              <span>
-                <FontAwesomeIcon icon={faSolarPanel} />
-              </span>
-              Admin panel
-            </Link>
-          </li>
+          {userRole === "ADMIN" && (
+            <li>
+              <Link href="/p-admin">
+                <span>
+                  <FontAwesomeIcon icon={faSolarPanel} />
+                </span>
+                Admin panel
+              </Link>
+            </li>
+          )}
         </ul>
         <img className="wave" src="/Images/wave.svg" alt="wave" />
       </aside>
